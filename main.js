090314e2a0537e227cb9f01e90afdeea6cd7ac35
Brant-Ma <brant.mazhiyuan@gmail.com>
2017-07-10 17:22:13 +0800

    page and script entry added

---
+++ main.js
@@ -0,0 +1,39 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+
+// keep a reference of window object
+let win
+
+function createWindow() {
+  // create window
+  win = new BrowserWindow({width: 800, height: 600})
+
+  // load index.html
+  win.loadURL(url.format({
+    pathname: path.resolve('.', 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  // open devtools
+  // win.webContents.openDevTools()
+
+  // release the reference of window object
+  win.on('closed', () => { win = null })
+}
+
+// event hook for application
+
+// ready
+app.on('ready', createWindow)
+
+// quit
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// start
+app.on('activate', () => {
+  if (win === null) createWindow()
+})
