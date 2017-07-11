979de9868925f7b223fd92717409c7ffb157e3d3
Brant-Ma <brant.mazhiyuan@gmail.com>
2017-07-11 15:49:44 +0800

    config file added

---
+++ config.js
@@ -0,0 +1,16 @@
+// import module
+const path = require('path')
+
+// export module
+module.exports = {
+  ROOT: __dirname,
+
+  APP_NAME: 'Beaker',
+  APP_SITE: 'https://github.com/Brant-Ma/Beaker',
+  APP_ICON: path.join(__dirname, 'static', 'icon.png'),
+
+  PAGE_DATA: path.join(__dirname, 'static', 'data.json'),
+  PAGE_INDEX: 'file://' + path.join(__dirname, 'renderer', 'index.html'),
+
+  AUTHOR_PROFILE: 'https://github.com/Brant-Ma'
+}
